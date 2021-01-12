e1494297142d152bd877ad6ee9aa785162db22a7
Adam Metcalf <adam.s.metcalf@outlook.com>
2021-01-12 10:19:55 -0500

    first commit

---
+++ WHITE-BADGE/JavaScript-Library/JavaScript-Basics/01-numbers.js
@@ -0,0 +1,24 @@
+console.log(1);
+console.log(1 + 1);
+console.log(1.1);
+console.log(1.1 * 1.1);
+
+//basic operators
+console.log(5 + 5); // Addition operator (+)
+console.log(6 - 6); // Subtraction operator (-)
+console.log(7 * 7); // Multiplication operator (*)
+console.log(8 / 8); // Division operator (/)
+console.log(2 ** 3); // Exponentiation operator (**)
+
+//modulus
+
+console.log(10 % 5);
+console.log(10 % 4);
+console.log(10 % 9);
+
+//Practice
+
+var z = 10;
+console.log(z);
+z+=1;
+console.log(z);
