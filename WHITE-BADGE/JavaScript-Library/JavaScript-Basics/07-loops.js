24385a41b612c073a3ad04c2be745250d79bfe13
Adam Metcalf <adam.s.metcalf@outlook.com>
2021-01-25 10:24:38 -0500

    final prework

---
+++ WHITE-BADGE/JavaScript-Library/JavaScript-Basics/07-loops.js
@@ -0,0 +1,10 @@
+for(let i = 1; i <= 100; i+= 1){
+  console.log(i);
+}
+
+function sayNumber(num){
+  console.log(num);
+}
+for(var i = 0;i<25;i++){
+  sayNumber(i);
+}
---
+++ WHITE-BADGE/JavaScript-Library/JavaScript-Basics/08-arrays.js
@@ -0,0 +1,22 @@
+let fullName = "Steve Stefanopoulous";
+let yearsOld = 37;
+let steve = {
+  name: fullName,
+  age: yearsOld,
+  sport: 'fencing',
+  pet: 'direwolf',
+  numberOfToothbrushes: 15,
+};
+
+console.log(fullName);
+console.log(yearsOld);
+console.log("");
+console.log(steve.name);
+console.log(steve.age);
+console.log(steve.sport);
+console.log(steve.pet);
+console.log(steve.numberOfToothbrushes);
+
+function helloWorldAgain() {
+  console.log("Hello World!");
+}
